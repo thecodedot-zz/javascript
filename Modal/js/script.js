ba84b94f478bfa89f606b50e460336e24c376ee5
thecodedot <72885755+thecodedot@users.noreply.github.com>
2022-11-26 02:36:35 +0500

    add escape btn for close modal

--- Modal/js/script.js
+++ Modal/js/script.js
@@ -6,7 +6,6 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 
 const openModal = function () {
-    console.log('Button Clicked')
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
 }
@@ -21,5 +20,12 @@ for (let i = 0; i < btnOpenModal.length; ++i) {
 }
 
 btnCloseModal.addEventListener('click', closeModal);
-
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+    console.log(e.key);
+
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();    
+    }
+});
