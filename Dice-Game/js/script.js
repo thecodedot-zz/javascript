676d8cbe66d89a3b7a753925f53da26b11f4bba0
thecodedot <72885755+thecodedot@users.noreply.github.com>
2022-11-26 14:04:37 +0500

    add starting condition

--- Dice-Game/js/script.js
+++ Dice-Game/js/script.js
@@ -8,3 +8,7 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+//Starting conditions
+score0El.textContent  = 0;
+score1El.textContent = 0;
+dice0El.classList.add('hidden');
