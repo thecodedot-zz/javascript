7dfe5cd0d8aa68b08d444f88ccc0ac92418a65fd
thecodedot <72885755+thecodedot@users.noreply.github.com>
2022-11-26 14:00:54 +0500

    add selector elements

--- Dice-Game/js/script.js
+++ Dice-Game/js/script.js
@@ -1,1 +1,10 @@
 'use strict'
+
+//Selecting Elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelectorById('score--1');
+const dice0El = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
